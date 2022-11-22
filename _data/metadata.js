4ee077b494931e78a96a8139a7b1c9531f689ec3
Jan T. Sott <git@idleberg.com>
2022-11-22 09:36:28 +0100

    update home url

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 let data = {
 	username: "visbot", // No leading @ here
 	homeLabel: "visbot.net",
-	homeUrl: "https://visbot.net/",
+	homeUrl: "https://visbot.github.io/twitter/",
 };
 
 data.avatar = `https://v1.indieweb-avatar.11ty.dev/${encodeURIComponent(data.homeUrl)}/`;
